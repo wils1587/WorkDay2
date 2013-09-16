0a331a4ddfe0fb76a7ab502b9458c391cb3ee33c
wils1587 <wils1587@umn.edu>
2013-09-16 15:24:35 -0500

    In Class Commit

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,19 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim FullName As String = txtFullName.Text 'input from user
+        Dim FirstName As String
+        Dim x As Integer
+
+        FullName = FullName.Trim 'Eliminates additional spaces before or after 
+
+        x = FullName.IndexOf(" ") 'Location of first space
+        FirstName = FullName.Substring(0, x) 'Substring of first name only
+
+        txtFirstName.Text = FirstName 'Displays substring in first name box
+    End Sub
+
+    Private Sub txtFullName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtFullName.TextChanged
 
     End Sub
 End Class
--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -1,8 +1,21 @@
 ﻿Option Strict On
 
 Public Class Form1
+    Dim FirstNumber As Double
+    Dim SecondNumber As Double
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        FirstNumber = CDbl(txtFirstNum.Text)
+        SecondNumber = CDbl(txtSecondNum.Text)
 
+        If FirstNumber > SecondNumber Then
+            txtResult.Text = txtFirstNum.Text
+        End If
+
+        If FirstNumber < SecondNumber Then
+            txtResult.Text = txtSecondNum.Text
+        End If
     End Sub
+
+   
 End Class
